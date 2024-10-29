a8649f0f454b3d2af0db387e860e535b8d33ec5b
EddieHowlett <50075688@belfastmet.ac.uk>
2024-10-29 10:23:20 +0000

    Update CSL Task 3.py

--- Strings/CSL Task 3.py
+++ Strings/CSL Task 3.py
@@ -1,12 +1,27 @@
 student_marks = float(input("Please enter your grade to get your result: "))
 print("the grade you have achieved is: ")
+if student_marks = <= 100 or >= 90:
+print("A")
+elif student_marks = <=89 or >= 80:
+print("B")
+elif student_marks = <= 79 or >= 70:
+print("C")
+elif student_marks = <=69 or >=60:
+print("D")
+else student_marks = <=59 or >=0:
+print("F")
 
 
+student_marks = float(input("Please enter your grade to get your result: "))
+print("The grade you have achieved is: ")
 
-
-
-
-
-
-Write a program that takes in a student’s test score (out of 100) and returns
-their letter grade based on the following scale: 90-100=A, 80-89 =B, 70 – 79 =C, 60-69=D and below 60=F
+if 90 <= student_marks <= 100:
+    print("A")
+elif 80 <= student_marks < 90:
+    print("B")
+elif 70 <= student_marks < 80:
+    print("C")
+elif 60 <= student_marks < 70:
+    print("D")
+elif 0 <= student_marks < 60:
+    print("F")
