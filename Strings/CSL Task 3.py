4a298241d2b64484477029f1dfe862329f2b2383
EddieHowlett <50075688@belfastmet.ac.uk>
2024-10-29 10:42:46 +0000

    Update CSL Task 3.py

--- Strings/CSL Task 3.py
+++ Strings/CSL Task 3.py
@@ -1,18 +1,4 @@
 student_marks = float(input("Please enter your grade to get your result: "))
-print("the grade you have achieved is: ")
-if student_marks = <= 100 or >= 90:
-print("A")
-elif student_marks = <=89 or >= 80:
-print("B")
-elif student_marks = <= 79 or >= 70:
-print("C")
-elif student_marks = <=69 or >=60:
-print("D")
-else student_marks = <=59 or >=0:
-print("F")
-
-
-student_marks = float(input("Please enter your grade to get your result: "))
 print("The grade you have achieved is: ")
 
 if 90 <= student_marks <= 100:
