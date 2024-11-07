64de36bd59f7588f0c54737904d16f71516ff62a
Eddie <50075688@belfastmet.ac.uk>
2024-11-07 15:05:29 +0000

    Function

--- Strings/Functions/Function example.py
+++ Strings/Functions/Function example.py
@@ -1,3 +1,8 @@
+#1. Ask the user for their hourly rate
+#2. Ask the user for the number of hours worked
+#3. Multiply 1 and 2 to gt the pay of the employee
+#4. Return value in 3
+
 def employee_pay():
     hourly_rate = float(input(f'Please enter your hourly rate, eg 10.59: '))
     num_hours_worked = float(input(f'Please enter your number of hours worked, eg 40: '))
@@ -20,11 +25,6 @@ def average_age(ages_list:list):
 list_ages_1 = [20, 21, 24, 13,15, 19, 22]
 ave_age = average_age(list_ages_1)
 print(f'The average age is {ave_age} years')
-
-#1. Ask the user for their hourly rate
-#2. Ask the user for the number of hours worked
-#3. Multiply 1 and 2 to gt the pay of the employee
-#4. Return value in 3
 
 empy_pay = employee_pay()
 print(f'Employee pay is {empy_pay}')
